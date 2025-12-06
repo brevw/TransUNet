87327d7f0ec91d24ff62e4e6d8068a26a51c8794
Ahmed Tlili <tlili.ahmed.mo@gmail.com>
2025-12-06 20:20:07 +0100

    transunet model structure

---
+++ models/TransUNet.py
@@ -0,0 +1,12 @@
+import torch
+from torch import nn
+
+
+class TransUNet(torch.nn.Module):
+    def __init__(self, in_channels: int, out_channels: int):
+        ...
+
+    def forward(self, x):
+        ...
+
+
